1ffc816595a29f8c7cfffb38ce8b7f96b1cf4593
Logesh G <logeshg5@gmail.com>
2024-11-22 19:28:02 +0530

    Format with prettier

--- format-document.ts
+++ format-document.ts
@@ -1,5 +1,14 @@
-import { editor } from "@silverbulletmd/silverbullet/syscalls";
+import { editor, shell, system } from "@silverbulletmd/silverbullet/syscalls";
 
 export async function formatDocument() {
-  await editor.flashNotification("Hello world!");
+  const pageName = await editor.getCurrentPage();
+  await editor.flashNotification("Formatting... Wait for page to reload");
+  //
+  const { code } = await shell.run("prettier", ["-w", pageName + ".md"]);
+  console.log("format document result ", code);
+  try {
+  } catch {
+    // We can ignore, this happens when there's no changes to commit
+  }
+  console.log("Done!");
 }
